35153f3bc75845df775e04949bbb937f632c5248
your name goes here <133010759+77889itsme@users.noreply.github.com>
2024-10-30 15:15:46 -0400

    src init change

--- src/tictactoe_yl5733/__init__.py
+++ src/tictactoe_yl5733/__init__.py
@@ -1,3 +1,9 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("tictactoe_yl5733")
+
+from .tictactoe_yl5733 import initialize_board, make_move, check_winner, reset_game
+__all__ = ["initialize_board", 
+           "make_move", 
+           "check_winner", 
+           "reset_game"]
